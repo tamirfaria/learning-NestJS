a9e324ba783300cca5d009a7f50a40c3b6bd811d
Lucas Tamir Faria <lucastamir@gmail.com>
2023-06-27 20:01:42 -0300

    :recycle: Typing user body params

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,24 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
+import { randomUUID } from 'crypto';
 import { UserRepository } from './user.repository';
+
+interface UserParams {
+  name: string;
+  email: string;
+  password: string;
+}
 
 @Controller('/user')
 export class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async createUser(@Body() userParams) {
-    this.userRepository.saveUser({ ...userParams, id: randomUUID() });
+  async createUser(@Body() userParams: UserParams) {
+    const parseParams = { ...userParams, id: randomUUID() };
+    this.userRepository.saveUser(parseParams);
     return {
       message: 'User created successfully.',
-      user: userParams,
+      user: parseParams,
     };
   }
 
